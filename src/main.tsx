db67b0535a58cdfe8502d3d07aef1e8349418a5d
Alberto-LL <Lautaroalbertolier@gmail.com>
2025-07-16 16:35:57 -0300

    base proyect setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './static/css/index.css'
+import App from './pages/mainPage/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
